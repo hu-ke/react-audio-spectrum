a02961ed57915685b692e29e329088d4225b1d86
HU KE <403392669@qq.com>
2024-06-24 17:45:25 +0800

    misc: add publicPath

--- exmaple/react-audio-spectrum-demo/src/App.js
+++ exmaple/react-audio-spectrum-demo/src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
     this.audioEle = null
     this.audioEle1 = null
     this.audioEle2 = null
-    this.playlist = [`${boydontcry}`, `${HowLongWillILoveYou}`, `${CantStandTheRain}`, `${process.env.PUBLIC_URL}${HoneyHoney}`]
+    this.playlist = [`${boydontcry}`, `${HowLongWillILoveYou}`, `${CantStandTheRain}`, `${HoneyHoney}`]
     this.audioEle3 = new Audio(this.playlist[3]);
     this.playInfoList = [{
       song: '男孩别哭',
